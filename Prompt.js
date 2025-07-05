fd8ae7f26e2ab4c451054b426ecf37fa115e517b
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2025-07-06 00:48:36 +0530

    Update Prompt.js

--- Prompt.js
+++ Prompt.js
@@ -1,1 +1,32 @@
+Prompt Engineering
+Amazon code whisperer Q
+1. AI for Code Completion
+
+Divide & Conquer the problem for big project like ecommerce website. ask for navbar , footer ,hero to build it not ask for whole ecomerce website.
+
+2. AI for debugging code & optimize
+
+* Here is my Python code. It has a bug. Please analyze the code,
+identify the bug, and suggest a fix. If possible, explain the
+issue in simple terms for me to understand.
+
+3. AI For problem solving
+
+* Give me a step-by-step solution to this problem statement
+and then challenge me with a harder version
+of the problem.
+
+* Explain this algorithm or data structure in simple terms.
+Then show me a real-world example
+where this is used.
+
+4. AI For project building
+
+* Help me brainstorm project ideas in Al for
+Driving cars. Suggest features and tools
+to implement these ideas.
+* 
+
+
+
 
